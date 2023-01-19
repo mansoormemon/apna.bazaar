5f37ecfd2239371f247766e52f085c027088e4ca
Mansoor Ahmed Memon <mansoorahmed.one@gmail.com>
2023-01-19 20:39:37 +0500

    Finalize onboarding screen

--- app/src/main/java/xyz/apna/bazaar/OnboardingActivity.java
+++ app/src/main/java/xyz/apna/bazaar/OnboardingActivity.java
@@ -1,7 +1,5 @@
 package xyz.apna.bazaar;
 
-//import android.content.SharedPreferences;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +8,19 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Objects;
+
 import xyz.apna.bazaar.ui.onboarding.ViewPagerAdapter;
+import xyz.apna.bazaar.ui.onboarding.ViewPagerModelObject;
 
 public class OnboardingActivity extends AppCompatActivity {
 
     protected ViewPager viewPager;
+    protected TabLayout pageIndicator;
 
-    protected Button btnNext;
+    protected Button btnGetStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +28,45 @@ public class OnboardingActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_onboarding);
 
-//        SharedPreferences.Editor preferencesEditor =
-//                getSharedPreferences(getPackageName(), MODE_PRIVATE).edit();
+//        SharedPreferences.Editor preferencesEditor = getSharedPreferences(getPackageName(), MODE_PRIVATE).edit();
 //        preferencesEditor.putBoolean(getString(R.string.key_is_first_session), true);
 //        preferencesEditor.apply();
 
         viewPager = findViewById(R.id.OA_VP_slides);
         ViewPagerAdapter adapter = new ViewPagerAdapter(this);
         viewPager.setAdapter(adapter);
-
-        btnNext = findViewById(R.id.OA_BTN_next);
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OnboardingActivity.this, MainActivity.class);
-                startActivity(intent);
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                final int LAST_PAGE_INDEX = ViewPagerModelObject.values().length - 1;
+                btnGetStarted.setVisibility(position == LAST_PAGE_INDEX ? View.VISIBLE : View.INVISIBLE);
             }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        pageIndicator = findViewById(R.id.OA_TL_pageIndicator);
+        for (View v : pageIndicator.getTouchables()) {
+            v.setClickable(false);
+        }
+        pageIndicator.setupWithViewPager(viewPager);
+
+        for (int i = 0; i < pageIndicator.getTabCount(); ++i) {
+            Objects.requireNonNull(pageIndicator.getTabAt(i)).setText(R.string.empty_string);
+        }
+
+        btnGetStarted = findViewById(R.id.OA_BTN_getStarted);
+        btnGetStarted.setOnClickListener(v -> {
+            Intent intent = new Intent(OnboardingActivity.this, MainActivity.class);
+            startActivity(intent);
         });
     }
 }
