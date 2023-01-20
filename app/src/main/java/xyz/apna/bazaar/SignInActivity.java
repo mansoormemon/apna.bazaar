300704e0aaa928654e5badc89bd3e2d790e893eb
Mansoor Ahmed Memon <mansoorahmed.one@gmail.com>
2023-01-20 19:50:05 +0500

    Implement sign up page

--- app/src/main/java/xyz/apna/bazaar/SignInActivity.java
+++ app/src/main/java/xyz/apna/bazaar/SignInActivity.java
@@ -3,14 +3,20 @@ package xyz.apna.bazaar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class SignInActivity extends AppCompatActivity {
-    protected Button btnSignIn;
+    protected TextInputEditText boxEmail;
+    protected TextInputEditText boxPassword;
     protected TextView linkForgotPassword;
+    protected Button btnSignIn;
+    protected ImageButton btnGoogle;
     protected TextView linkSignUp;
 
     @Override
@@ -19,13 +25,20 @@ public class SignInActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_sign_in);
 
-        btnSignIn = findViewById(R.id.SIA_BTN_signIn);
-        btnSignIn.setOnClickListener(v -> {
-            Toast.makeText(this, "Sign In", Toast.LENGTH_SHORT).show();
-        });
+        boxEmail = findViewById(R.id.SIA_TIET_email);
+        boxPassword = findViewById(R.id.SIA_TIET_password);
 
         linkForgotPassword = findViewById(R.id.SIA_TV_forgotPassword);
         linkForgotPassword.setOnClickListener(v -> Toast.makeText(this, "Stub: Forgot Password", Toast.LENGTH_SHORT).show());
+
+        btnSignIn = findViewById(R.id.SIA_BTN_signIn);
+        btnSignIn.setOnClickListener(v -> {
+        });
+
+        btnGoogle = findViewById(R.id.SIA_IBTN_google);
+        btnGoogle.setOnClickListener(v -> {
+            Toast.makeText(this, "Stub: Sign in with Google", Toast.LENGTH_SHORT).show();
+        });
 
         linkSignUp = findViewById(R.id.SIA_TV_signUp);
         linkSignUp.setOnClickListener(v -> {
--- app/src/main/java/xyz/apna/bazaar/SignUpActivity.java
+++ app/src/main/java/xyz/apna/bazaar/SignUpActivity.java
@@ -1,15 +1,56 @@
 package xyz.apna.bazaar;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUpActivity extends AppCompatActivity {
+    protected TextInputEditText boxEmail;
+    protected TextInputEditText boxPassword;
+    protected RadioGroup radioGender;
+    protected TextInputEditText boxContact;
+    protected CheckBox ckbLegal;
+    protected Button btnSignUp;
+    protected ImageButton btnGoogle;
+    protected TextView linkSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_sign_up);
+
+        boxEmail = findViewById(R.id.SUA_TIET_email);
+
+        boxPassword = findViewById(R.id.SUA_TIET_password);
+
+        radioGender = findViewById(R.id.SUA_RG_gender);
+
+        boxContact = findViewById(R.id.SUA_TIET_contact);
+
+        ckbLegal = findViewById(R.id.SUA_CKB_legal);
+
+        btnSignUp = findViewById(R.id.SUA_BTN_signUp);
+
+        btnGoogle = findViewById(R.id.SUA_IBTN_google);
+        btnGoogle.setOnClickListener(v -> {
+            Toast.makeText(this, "Stub: Sign in with Google", Toast.LENGTH_SHORT).show();
+        });
+
+        linkSignIn = findViewById(R.id.SUA_TV_signIn);
+        linkSignIn.setOnClickListener(v -> {
+            Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+            startActivity(intent);
+        });
     }
+
 }
