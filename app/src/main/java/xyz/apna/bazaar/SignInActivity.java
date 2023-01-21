b4611a3547b5d5fb6feb47b78e3cc80b097016c7
Mansoor Ahmed Memon <mansoorahmed.one@gmail.com>
2023-01-21 16:28:00 +0500

    Implement google sign in

--- app/src/main/java/xyz/apna/bazaar/SignInActivity.java
+++ app/src/main/java/xyz/apna/bazaar/SignInActivity.java
@@ -9,6 +9,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class SignInActivity extends AppCompatActivity {
@@ -19,11 +25,17 @@ public class SignInActivity extends AppCompatActivity {
     protected ImageButton btnGoogle;
     protected TextView linkSignUp;
 
+    protected GoogleSignInOptions gso;
+    protected GoogleSignInClient gsc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_sign_in);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this, gso);
 
         boxEmail = findViewById(R.id.SIA_TIET_email);
         boxPassword = findViewById(R.id.SIA_TIET_password);
@@ -37,7 +49,8 @@ public class SignInActivity extends AppCompatActivity {
 
         btnGoogle = findViewById(R.id.SIA_IBTN_google);
         btnGoogle.setOnClickListener(v -> {
-            Toast.makeText(this, "Stub: Sign in with Google", Toast.LENGTH_SHORT).show();
+            Intent signInIntent = gsc.getSignInIntent();
+            startActivityForResult(signInIntent, 1000);
         });
 
         linkSignUp = findViewById(R.id.SIA_TV_signUp);
@@ -45,5 +58,21 @@ public class SignInActivity extends AppCompatActivity {
             Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                startActivity(intent);
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/xyz/apna/bazaar/SignUpActivity.java
+++ app/src/main/java/xyz/apna/bazaar/SignUpActivity.java
@@ -1,17 +1,27 @@
 package xyz.apna.bazaar;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
+    final int MIN_PASSWORD_LEN = 8;
+
     protected TextInputEditText boxEmail;
     protected TextInputEditText boxPassword;
     protected RadioGroup radioGender;
@@ -19,6 +29,9 @@ public class SignUpActivity extends AppCompatActivity {
     protected CheckBox ckbLegal;
     protected Button btnSignUp;
     protected TextView linkSignIn;
+
+    protected FirebaseAuth auth;
+    protected FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +50,41 @@ public class SignUpActivity extends AppCompatActivity {
         ckbLegal = findViewById(R.id.SUA_CKB_legal);
 
         btnSignUp = findViewById(R.id.SUA_BTN_signUp);
+        ProgressDialog progressDialog = new ProgressDialog(this);
+
+        FirebaseApp.initializeApp(getBaseContext());
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        btnSignUp.setOnClickListener(v -> {
+            String email = Objects.requireNonNull(boxEmail.getText()).toString();
+            String password = Objects.requireNonNull(boxPassword.getText()).toString();
+            String contact = Objects.requireNonNull(boxContact.getText()).toString();
+
+            if (!email.matches(String.valueOf(Patterns.EMAIL_ADDRESS))) {
+                boxEmail.setError("Enter correct email");
+            } else if (password.isEmpty() || password.length() < MIN_PASSWORD_LEN) {
+                boxPassword.setError("Enter correct password");
+            } else {
+                progressDialog.setMessage("Registering...");
+                progressDialog.setTitle("Registration");
+                progressDialog.setCanceledOnTouchOutside(false);
+                progressDialog.show();
+
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        progressDialog.dismiss();
+                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        Toast.makeText(SignUpActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(SignUpActivity.this, String.valueOf(task.getException()), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
         linkSignIn = findViewById(R.id.SUA_TV_signIn);
         linkSignIn.setOnClickListener(v -> {
@@ -44,5 +92,4 @@ public class SignUpActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
-
 }
